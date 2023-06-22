0fdb9b8f38c2e8503dd89fac99c6cfc09dde1354
khardatt <63426782+khardatt@users.noreply.github.com>
2023-06-22 17:07:56 -0400

    Update streamlit_app.py
    add record to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,4 @@ streamlit.dataframe(my_data_row)
 
 add_fruit = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user added ', add_fruit)
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
