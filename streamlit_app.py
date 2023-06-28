552ad1784a8fb6242fafccfbb8dc5ecf367ac70a
khardatt <63426782+khardatt@users.noreply.github.com>
2023-06-28 15:42:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
 
 """
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+#streamlit.header('Breakfast Menu')
+#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+#streamlit.text('🥗 Kale, Spinach & rocket Smoothie')
+#streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+#streamlit.text('🥑🍞 Avocado Toast')
 """
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
